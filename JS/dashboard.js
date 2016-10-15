dbd4820e0743c2025215e00f8b79b465d476d4ca
Shivani Gupta <shivaniguptabca@gmail.com>
2016-10-16 02:36:53 +0530

    Pushing Changes

--- JS/dashboard.js
+++ JS/dashboard.js
@@ -1,14 +1,21 @@
 $(document).ready( function(){
 	var showDialog = document.getElementById('showDialog');
 	var infoModal = document.getElementById('infoModal');
+	
 	//To display  information modal window 
 	showDialog.addEventListener('click', function(e) {
 		infoModal.showModal();
 	});
-	//To close information modal window 
+	//To close information modal window by clicking modal close button
 	var modalClose = document.getElementById('modalClose');
 	modalClose.addEventListener('click', function(e) {
 		infoModal.close();
 	});
-
+	
+	//To close information modal window by clicking anywhere outside the modal
+	window.addEventListener('click', function(e) {
+		if (e.target == infoModal) {
+			infoModal.close();
+		}
+	});
 });
